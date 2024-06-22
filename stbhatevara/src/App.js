daf5250e6e76945d559d32622134e3d2d4fe7850
sneha <snehameruva003@gmail.com>
2024-06-22 20:15:05 +0530

    homepage

--- stbhatevara/src/App.js
+++ stbhatevara/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import SignupPage from './components/SignupPage';
 import './App.css';
+import Home from './components/Home'
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LoginPage />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </div>
---
+++ stbhatevara/src/components/Home.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css'; // Correct path to slick.css
+import 'slick-carousel/slick/slick-theme.css'; // Correct path to slick-theme.css
+import './Home.css';
+
+const Home = () => {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <div className="Home">
+      <header className="header">
+        <Slider {...sliderSettings}>
+          <div><img src="/images/image1.jpg" alt="Slide 1" /></div>
+          <div><img src="/images/image2.jpg" alt="Slide 2" /></div>
+          <div><img src="/images/image3.jpg" alt="Slide 3" /></div>
+        </Slider>
+        <h1>NGO Organization</h1>
+      </header>
+
+      <section className="content">
+        <div className="division" id="student">
+          <h2>Student</h2>
+          <p>Information about student involvement...</p>
+          <Link to="/login">Login</Link> {/* Link to login page */}
+        </div>
+        <div className="division" id="volunteer">
+          <h2>Volunteer</h2>
+          <p>Information about volunteering...</p>
+        </div>
+        <div className="division" id="ngo">
+          <h2>NGO</h2>
+          <p>Information about the NGO...</p>
+        </div>
+        <div className="division" id="trust">
+          <h2>Trust</h2>
+          <p>Information about trust...</p>
+        </div>
+        <div className="division" id="alumni">
+          <h2>Alumni</h2>
+          <p>Information about alumni...</p>
+        </div>
+      </section>
+
+      <footer className="footer">
+        <p>&copy; 2024 stbhatevaraorg. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
