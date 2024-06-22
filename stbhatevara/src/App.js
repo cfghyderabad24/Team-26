6507dd29d0e06398d685d4bff903941c39218072
tribhuvanasree <tribhuvanasree@gmail.com>
2024-06-22 22:21:36 +0530

    alumniContent

--- stbhatevara/src/App.js
+++ stbhatevara/src/App.js
@@ -1,25 +1,29 @@
-// src/App.js
+// App.js
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import TrusteeLevel from './TrusteeLevel/main';
 import SelectionRecord from './TrusteeLevel/SelectionRecord';
 import Analytics from './TrusteeLevel/Analytics';
 import Status from './TrusteeLevel/Status'; // Updated import
+import AlumniHome from './alumniLevel/AlumniHome';
+import AlumniProfile from './alumniLevel/AlumniProfile';
+import AlumniConnections from './alumniLevel/AlumniConnections';
 
-function App() {
+const App = () => {
     return (
         <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<TrusteeLevel />} />
+            <Routes>
+                <Route path="/" element={<Navigate to="/alumni/home" />} />
+                <Route path="/alumni/home" element={<AlumniHome />} />
+                <Route path="/alumni/profile" element={<AlumniProfile />} />
+                <Route path="/alumni/connections" element={<AlumniConnections />} />
+                <Route path="/" element={<TrusteeLevel />} />
                     <Route path="/selection-record" element={<SelectionRecord />} />
                     <Route path="/analytics" element={<Analytics />} />
-                    <Route path="/status/:id" element={<Status />} /> {/* New route for Status component */}
-                </Routes>
-            </div>
+                    <Route path="/status/:id" element={<Status />} />            </Routes>
         </Router>
     );
-}
+};
 
-export default App;
+
