9229ac9ffe59a6e496e621b567f1223abdbb8891
NavikaReddy <navikareddy12@gmail.com>
2024-06-22 18:31:37 +0530

    login

--- stbhatevara/APIs/UserApi.js
+++ stbhatevara/APIs/UserApi.js
@@ -1,57 +1,68 @@
-const express= require('express')
-const User = require('../models/User')
-const app = express()
-const bcryptjs = require('bcryptjs')
-app.use(express.json())
-app.post('/signup',async(req,res)=>{
+const express = require('express');
+const User = require('../models/User');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+    User
+        .find()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => {
+            res.status(500).json(err);
+        });
+});
+
+
+
+app.post('/signup', async (req, res) => {
     const newUser = req.body;
-    console.log(newUser)
-    let user= await User
-    .findOne({username:newUser.username})
-    if(user!=null)
-        {
-            res.status(400).json({msg:"user already exists"})
+    console.log(newUser);
+    try {
+        let user = await User.findOne({ username: newUser.username });
+        if (user != null) {
+            res.status(400).json({ msg: "User already exists" });
+        } else {
+            //let hashPass = await bcryptjs.hash(newUser.password, 10); // hash password
+            //newUser.password = hashPass;
+            const newActUser = new User(newUser);
+            await newActUser.save();
+            res.status(200).json({ msg: "User created successfully" });
         }
-    else
-    {
-        let hashPass= await bcryptjs.hash(newUser.password,5);
-        newUser.password=hashPass
-        const newActUser = new User(newUser);
-        await newActUser.save();
-        res.status(200).json(newUser);
+    } catch (err) {
+        res.status(500).json({ msg: "Error creating user", error: err });
     }
-})
+});
 
-app.post("/signin", async (req, res) => {
-    const { success } = signinBody.safeParse(req.body)
-    if (!success) {
-        return res.status(411).json({
-            message: "Incorrect inputs"
-        })
+app.post('/login', async (req, res) => {
+    const userCredObj = req.body;
+    try {
+        let userOfDb = await User.findOne({ username: userCredObj.username });
+        if (userOfDb == null) {
+            res.status(400).json({ msg: "Invalid username" });
+        } 
+        else {
+            console.log(userCredObj)
+            console.log(userOfDb)
+            if (userOfDb.password!=userCredObj.password) {
+                res.status(400).json({ message: "Wrong Password" });
+            } else {
+                let jwtToken = jwt.sign({ username: userOfDb.username }, process.env.SECRET_KEY, { expiresIn: "2 days" });
+                delete userOfDb.password;
+                res.status(200).json({ message: "Success", token: jwtToken, user: userOfDb });
+            }
+        }
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({ msg: "Error logging in", error: err });
     }
+});
 
-    const user = await User.findOne({
-        username: req.body.username,
-        password: req.body.password
-    });
-
-    if (user) {
-        const token = jwt.sign({
-            userId: user._id
-        }, JWT_SECRET);
-  
-        res.json({
-            token: token
-        })
-        return;
-    }
-
-    
-    res.status(411).json({
-        message: "Error while logging in"
-    })
-})
-
-
-
-module.exports=app;
+module.exports = app;
